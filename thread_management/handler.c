ed522abb12d12d05ac330c01d2efa86818707c8a
Deverick-Simpson <deverick@me.com>
2014-11-05 19:15:53 -0600

    t7gt7

--- thread_management/handler.c
+++ thread_management/handler.c
@@ -6,19 +6,13 @@
 
 #define ALARMTICKS(x) ((alt_ticks_per_second()*(x))/10)
 alt_alarm alarm;
-int global_flag =0;
 
-
-static int num_threads=8;
+int x = 10;			//This value defines the paramater for ALARMTICKS(10)
+int MAX = 500000;
+int global_flag =0;		// 1 means timer interrupt 0 not
+static int num_threads=8;  //This defines the max number of current threads possible
 int num_currThreads =0;
-   
-   
-void destroyEntry(Entry entry)
-{
-    free(entry);
-}
-
-
+int globalFramePointer = 0;
 
  // disable an interrupt
  #define DISABLE_INTERRUPTS() {  \
@@ -30,19 +24,19 @@ void destroyEntry(Entry entry)
    asm("movi et, 1");          \
    asm("wrctl status, et");    \
  }
-<<<<<<< HEAD
  
- 
- 
- 
-=======
-struct TCBQueue{
+
+struct {
     struct int front;
     struct int rear;
     int count;
-    ThreadControlBlock items[max];
-};
+    ThreadControlBlock items[num_threads];
+}TCBQueue;
 
+
+void resetFlag(){
+	global_flag = 0;
+}
 
 
 void initialize(struct queue q){
@@ -56,50 +50,59 @@ void dequeue(struct queue *q){
     ThreadControlBlock x;
     q->count=q->count-1;
     x=q->items[front];
-    q->front=(q->front+1)%max;
+    q->front=(q->front+1)%num_threads;
     return x;
 }
 
 
 
 void enqueue(struct queue *q,ThreadControlBlock x){
-    if(q->count==max){
+    if(q->count==num_threads){
         printf("%d is not inserted. Queue is " "full.\n",x);
     }else{
         q->count = q->count+1;
-        q->rear = (q->rear+1) % max;
+        q->rear = (q->rear+1) % num_threads;
         q->items[rear]=x;
     }
 }
 
-
-
 typedef struct {
 	int  thread_id;
 	int scheduling_status;			//..Running-1, Ready-2, waiting-3, start-4, done-5
-	int size;
-	(int*) malloc(size) context_pointer;
+	int stack_size;
+	int stack;
+	(int*) malloc(stack_size) context_pointer;
+	int frame_pointer;
 }ThreadControlBlock;
 
-
-//Create 8 threads 
-void mythread_create(int thread_id){
+	/*
 	ThreadControlBlock threadTest;	
 	threadTest.thread_id = thread_id;
 	threadTest.scheduling_status = 3;
-	threadTest.size = ; 		//Need to determine the size of the thread..dynamic right??s
+	threadTest.stack_size = stackSize; 		//Not dynamic needs to be the size of the allocation passe din
 	thread.context_pointer = ; //The address location of the context_pointer. 
+	*/
 	
+	
+
+//Create 8 threads 
+void mythread_create(int thread_id, int stackSize){
+
+	//So here is where we will store the stack contents from the context_pointer
+	//typecast the context_pointer to an integer arraylist. Store the offset from..such as, context_pointer[4]
+	
+
 }
 
 
 //Suspend main thread
 void mythread_join(ThreadControlBlock thread){
-    
-  free(thread);
+    //Place in queue
 
 }
 
+
+//Complete. This will be the running thread
 void mythread(int thread_id){
     //the declaration of j as an integer was added on 10/24/2011
     int i, j , n=0;
@@ -113,23 +116,17 @@ void mythread(int thread_id){
 
 
 
-//need to complete another test
-// are we going to call schedule in protoType_os?
-
-void prototype_os(thread arrauy, at array, st array)
+void prototype_os()
  {
-
-     
   for (i = 0; i < num_threads; i++)
      {
          // Here: do whatever you need
          // Here: call mythread_create so that the TCB for each thread is created    
          //assembly calls
-     
-         mythread_create(i);
+         newThread = mythread_create(i, 800);
      }
-    mythread_create(someArryOfThreads[i]);
-              }
+
+
   for (i = 0; i < num_threads; i++)
      {
          // Here: do whatever you need
@@ -137,10 +134,11 @@ void prototype_os(thread arrauy, at array, st array)
          mythread_join(thread);
      }
      // Here: initialize the timer and its interrupt handler as is done in Project I
+	 alt_alarm_start(&alarm,ALARMTICKS(x), mythread_handler, NULL);
+	 
      while (true)
      {
          alt_printf ("This is the OS prototype for my exciting CSE351 course projects!\n");
-		int MAX = 5;
         for (j = 0 ; j < MAX; j++);
      }
 }
@@ -149,30 +147,28 @@ void prototype_os(thread arrauy, at array, st array)
 
 alt_u32 mythread_handler(void *context){
 	//The global flag is used to indicate a timer interrupt
-	alt_printf("Interrupted by the timer handler!\n");
+	alt_printf("Interrupted by the mythread handler!\n");
 	global_flag = 1;
-	return ALARMTICKS(QUANTUM_LENGTH);
-	
+	return ALARMTICKS(x);
 }
 
 
 
-mythread_scheduler(void *param_list){
-
-=======
-//we need to parse in a arrive time list and a sevice time list, also should we just define the time slice as 1?
-mythread_scheduler(thread array, at array, st array){
-    
->>>>>>> FETCH_HEAD
+mythread_scheduler(void *context  //current sp){
+     TCBQueue threadQueue;
 //Here do whatever you need.
 //If there are still ready threads in the run queue
 //conditional flag checking of interrupt is from timer or not 
-	if(){
+	if(global_flag == 0  && threadQueue.count >0){
+		//Preserve context and restore that of the next to be execeuted
 		//Perform thread scheduling
+		
 	}else{
 		alt_printf("Interrupted by the DE2 timer!\n");
 	}
 }
+
+
 
 
 //After creating thread, inject assembly to
@@ -187,24 +183,10 @@ mythread_scheduler(thread array, at array, st array){
 //global varibale--is my button coming from timer or from interrupt handler.
 //When you come back is when you switch the stack space.
 
-
-
 	//main calling function
 	int main(void){
-        pthread_t threadArray;
-        // create a queue, create 8 threads and store them into the queue
-        int at,st;;
-        at=new int[8];
-        st=new int[8];
-        for(i=0;i<n;i++)
-        {
-            printf("Enter arrival time and sevice time for Process P%d :",i+1);
-            scanf("%d",&at[i]);
-            scanf("%d",&st[i]);            
-        }
 		//Calling prototype OS
-		prototype_os(threadArray,at,st);
-        
+		prototype_os();
 		return 0;
 	}
 } 
