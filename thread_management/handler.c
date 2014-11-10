d046dbd7b65655e44ac24a53912403d7dae7386d
Deverick-Simpson <deverick@me.com>
2014-11-10 12:21:29 -0600

    bunch of changes

--- thread_management/handler.c
+++ thread_management/handler.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/alt_alarm.h>
-
 #include "alt_types.h"
 
 #define ALARMTICKS(x) ((alt_ticks_per_second()*(x))/10)
@@ -15,6 +14,33 @@ static int num_threads=8;  //This defines the max number of current threads poss
 static int stackPointer;
 
 
+typedef struct {
+	int  thread_id;
+	int scheduling_status;			//..Running-1, Ready-2, waiting-3, start-4, done-5
+	int stack_size;
+	int context_pointer;
+	int frame_pointer;
+	int stack;
+}ThreadControlBlock;
+
+
+typedef struct{
+	int front;
+    int rear;
+    int count;
+    ThreadControlBlock items[8];
+}TCBQueue;
+
+//Global control block and queue
+ThreadControlBlock tempThread;
+TCBQueue mainThreadQueue;
+
+
+void initialize(TCBQueue emptyQueue){
+	emptyQueue.count =0;
+	emptyQueue.front = 0;
+	emptyQueue.rear = 0;
+}
 
 
  // disable an interrupt
@@ -28,23 +54,12 @@ static int stackPointer;
    asm("wrctl status, et");    \
  }
 
-
-struct{
-	int front;
-    int rear;
-    int count;
-    ThreadControlBlock items[num_threads];
-}TCBQueue;
-
-TCBQueue mainThreadQueue = NULL;
-
 void resetFlag(){
 	global_flag = 0;
 }
 
-
-
 int checkFlag(){
+
 	if(global_flag==1){
 		//returning new .context_pointer...where is it going? 
 		resetFlag();
@@ -59,83 +74,30 @@ void cleanup(){
 	 ENABLE_INTERRUPTS();
 }
 
-void initialize(struct queue *q){
-    q->count=0;
-    q->front=NULL;
-    q->rear=NULL;
-}
-
-typedef struct {
-	int  thread_id;
-	int scheduling_status;			//..Running-1, Ready-2, waiting-3, start-4, done-5
-	int stack_size;
-	(int*) malloc(stack_size) context_pointer;
-	int frame_pointer;
-	int stack;
-}ThreadControlBlock;
-
-ThreadControlBlock tempThread;
-
 
 ThreadControlBlock dequeue(){
-	struct queue *q = *mainThreadQueue;
-	--mainThreadQueue.count;
+	TCBQueue q = mainThreadQueue;
     ThreadControlBlock x;
-    q->count=q->count-1;
-    x=q->items[front];
-    q->front=(q->front+1)%num_threads;
+    q.count=q.count-1;
+    x= q.items[q.front];
+    q.front=(q.front+1)%num_threads;
+	mainThreadQueue.count = mainThreadQueue.count -1 ;
     return x;
 }
 
 
-
-void enqueue(ThreadControlBlock *x){
-	struct queue *q = *mainThreadQueue;
-    if(q->count==num_threads){
+void enqueue(ThreadControlBlock x){
+	 TCBQueue q = mainThreadQueue;
+    if(q.count==num_threads){
         printf("%d is not inserted. Queue is " "full.\n",x);
     }else{
-        q->count = q->count+1;
-        q->rear = (q->rear+1) % num_threads;
-        q->items[rear]=x;
-        ++mainThreadQueue.count;
+        q.count = q.count+1;
+        q.rear = (q.rear+1) % num_threads;
+        q.items[q.rear]=x;
+        mainThreadQueue = q;
+
     }
 }
-
-
-	
-
-//Create 8 threads 
-ThreadControlBlock *mythread_create(int thread_id, int stackSize){
-	//So here is where we will store the stack contents from the context_pointer
-	//typecast the context_pointer to an integer arraylist. Store the offset from..such as, context_pointer[4]
-	ThreadControlBlock *threadTest;	
-	threadTest = (tcb *)malloc(sizeof(tcb));
-	threadTest -> thread_id = thread_id;
-	threadTest.scheduling_status = 3;
-	threadTest.stack_size = stackSize; 		
-	threadTest.stack = malloc(sizeOf(int)*stackSize);
-	
-	
-	threadTest.context_pointer =  threadTest.stack+ stackSize - 19;	 //The address location of the context_pointer. aka stack pointer...RA
-	threadTest.frame_pointer = 	threadTest.context_pointer -1;
-	
-	
-	//Is this an ok way to offsey from context_pointer
-	//Create Stack and save sp
-	threadTest.context_pointer + 18 = mythread;		//EA Register---mythread
-	threadTest.context_pointer + 5 = thread_id; //R4 register for parameters		
-	threadTest.context_pointer + 17 = 1;		// eStatus sets the flag for a timer interrupt	
-	threadTest.context_pointer - 1 = threadTest.stack + stackSize; //current FP
-	return threadTest;
-}
-
-
-//Suspend main thread
-  //Place in queue
-void mythread_join(ThreadControlBlock thread){
-     enqueue(thread);	
-}
-
 
 //Complete. This will be the running thread
 void mythread(int thread_id){
@@ -143,27 +105,75 @@ void mythread(int thread_id){
     int i, j , n=0;
     n=(thread_id % 2 ==0)? 10:15;
     for(i=0;i<n; i++){
-        printf[1]("This is message %d of thread #%d.\n", i, thread_id); //Whats with the printf array
-        printf("This is message %d of thread #%d.\n", i, thread_id); 
+        printf("This is message %d of thread #%d.\n", i, thread_id); //Whats with the printf array
         for (j = 0; j < MAX; j++);
     }
 }
 
+	
+
+//Create 8 threads 
+ThreadControlBlock *mythread_create(int thread_id, int stackSize, void (mythread)(int thread_id)){
+	//So here is where we will store the stack contents from the context_pointer
+	//typecast the context_pointer to an integer arraylist. Store the offset from..such as, context_pointer[4]
+	int *localStack;
+	ThreadControlBlock *threadTest=malloc(sizeof(ThreadControlBlock));
+	threadTest->thread_id = thread_id;
+	threadTest->scheduling_status = 3;
+	threadTest->stack_size = stackSize;
+	threadTest->stack = (int *)malloc(sizeof(int) * stackSize);  //probably should not use
+	
+	
+	threadTest->context_pointer =  (int*)threadTest->stack+ stackSize - 19;	 //The address location of the context_pointer. aka stack pointer...RA
+
+	//Is this an ok way to offsey from context_pointer
+	//Create Stack and save sp
+	//edit
+	localStack =  threadTest->context_pointer;
+	localStack[0] = (int)cleanup;
+	localStack[5] =thread_id;
+	localStack[17] = 1;
+	localStack[18] = (int)mythread;
+	localStack[-1] =threadTest->context_pointer + threadTest->stack_size;
+
+
+	return threadTest;
+}
+
+
+//Suspend main thread
+  //Place in queue
+void mythread_join(ThreadControlBlock *thread){
+	ThreadControlBlock enqueueThread = *thread;
+     enqueue(enqueueThread);
+}
+
+
+
+alt_u32 mythread_handler(void * context){
+	//The global flag is used to indicate a timer interrupt
+	global_flag = 1;
+	alt_printf("Interrupted by the mythread handler!\n");
+
+	return ALARMTICKS(x);
+}
 
 
 void prototype_os()
 {
+	//thread initialized on creation..I want to bring back the initialize function
+ 	ThreadControlBlock *newThread;
  	initialize(mainThreadQueue);
- 	ThreadControlBlock *newThread;	
  	int i,j;
   for (i = 0; i < num_threads; i++)
      {
          // Here: do whatever you need
          // Here: call mythread_create so that the TCB for each thread is created    
          //assembly calls
-         newThread = mythread_create(i, 4096);
+         newThread = mythread_create(i, 4096, mythread);
          // Here: call mythread_join to make each thread runnable/ready
          mythread_join(newThread);
+
      }
         
      // Here: initialize the timer and its interrupt handler as is done in Project I
@@ -177,12 +187,7 @@ void prototype_os()
 }
 
 
-alt_u32 mythread_handler(void * context){
-	//The global flag is used to indicate a timer interrupt
-	alt_printf("Interrupted by the mythread handler!\n");
-	global_flag = 1;
-	return ALARMTICKS(x);
-}
+
 
 //If there are still ready threads in the run queue
 //conditional flag checking of interrupt is from timer or not 
@@ -195,7 +200,8 @@ void mythread_scheduler(void *context){
 		//Preserve context and restore that of the next to be execeuted
 		//Perform thread scheduling			
 	if(mainThreadQueue.count >0){
-		tempThread.context_pointer = *context;
+		alt_printf("Am I getting here!\n");
+		tempThread.context_pointer = (int*)context;
 		enqueue(tempThread);
 		tempThread=dequeue();
 		//Return the new thread stack pointer...when returning please access the .context_pointer property	
@@ -223,4 +229,4 @@ void mythread_scheduler(void *context){
 		prototype_os();
 		return 0;
 	}
-} 
+
